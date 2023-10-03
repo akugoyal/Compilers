2d027bcaed61ca70cf9009fbbc9a7534f230fbe5
akugoyal <63987908+akugoyal@users.noreply.github.com>
2023-10-02 19:39:11 -0700

    ParseFactor

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -28,18 +28,55 @@ public class Parser
     private int parseNumber() throws ScanErrorException
     {
         int num = Integer.parseInt(current.getToken());
-        eat(current.getToken());
+        eat(Integer.toString(num));
         return num;
     }
+
 
     public void parseStatement() throws ScanErrorException
     {
         if (current.getToken().equals("WRITELN")) {
             eat("WRITELN");
             eat("(");
-            System.out.println(parseNumber());
+            System.out.println(parseTerm());
             eat(")");
             eat(";");
         }
+        //eat("\n");
+        eat("EOF");
+    }
+
+    private int parseFactor() throws ScanErrorException
+    {
+        if (current.getToken().equals("(")) {
+            eat("(");
+            int factor = this.parseTerm();
+            eat(")");
+            return factor;
+        }
+        if (current.getToken().equals("-")) {
+            eat("-");
+            int factor = -1*parseTerm();
+            return factor;
+        }
+        return this.parseNumber();
+    }
+
+    private int parseTerm() throws ScanErrorException
+    {
+        int factor = parseFactor();
+        while(current.getToken().equals("*") || current.getToken().equals("/")) {
+            if (current.getToken().equals("*"))
+            {
+                eat("*");
+                factor *= parseFactor();
+            }
+            else if (current.getToken().equals("/"))
+            {
+                eat("/");
+                factor /= parseFactor();
+            }
+        }
+        return factor;
     }
 }
