0e9698a3152ffb3996a2065bf1a17acdd9912462
akugoyal <63987908+akugoyal@users.noreply.github.com>
2023-12-05 21:00:49 -0800

    Fibonacci documentation

--- Subroutines/fib.asm
+++ Subroutines/fib.asm
@@ -36,33 +36,31 @@ fib:
 subu $sp, $sp, 12
 sw $ra, ($sp)
 sw $a0, 4($sp)
-sw $s0, 8($sp)
+sw $s0, 8($sp)			# Push ra, a0, s0
 
-# Base Case
-blt $a0, 2, base
+blt $a0, 2, base		# Base case
 
-#-------------------
 subu $a0, $a0, 1
 jal fib	
-move $s0, $v0			#fib(n-1) into t0
+move $s0, $v0			# fib(n-1) into s0
 
 
 subu $a0, $a0, 1
-jal fib				#fib(n-2) into v0
-addu $v0, $v0, $s0
+jal fib				# fib(n-2) into v0
 
+addu $v0, $v0, $s0		# fib(n-1) + fib(n-2) into v0
 
 end:
-lw $s0, 8($sp)
 lw $ra, ($sp)
 lw $a0, 4($sp)
-addu $sp, $sp, 12	#fib(n-2) into s0
-
-jr $ra
+lw $s0, 8($sp)
+addu $sp, $sp, 12		# Pop ra, a0, s0
+jr $ra				# Return
 
 base:
 move $v0, $a0
-j end
+j end				# Return a0
+
 
 .data
 in: .asciiz "Enter a number:"
